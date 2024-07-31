830b31b7d6a3285a12d1af9f3e50296d66f309e9
sherelynn <sherelynnb@missionreadyhq.com>
2024-08-01 01:31:56 +1200

    Added a className in main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 // App.js
-import React from 'react';
-import ProductList from './Components/ProductList';
-import ShoppingCart from './Components/ShoppingCart';
-import './App.css'
+import React from "react"
+import ProductList from "./Components/ProductList"
+import ShoppingCart from "./Components/ShoppingCart"
+import "./App.css"
 const App = () => {
   return (
- 
-      <div>
-        <h1 className='app-heading'>E-Commerce Application</h1>
-        <ProductList />
-        <ShoppingCart />
-      </div>
+    <div className="app">
+      <h1 className="app-heading">E-Commerce Application</h1>
+      <ProductList />
+      <ShoppingCart />
+    </div>
+  )
+}
 
-  );
-};
-
-export default App;
+export default App
